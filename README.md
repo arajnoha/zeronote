2a3429e09f895904f1fbb74783c55e1bdf8a4894
Adam Rajnoha <adam@rajnoha.com>
2021-09-22 23:20:23 +0200

    even more minimal, stripping w.php

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # zeronote
-Ultra minimalist php flat-file notetaking app below 1kB  
+Ultra minimalist php flat-file notetaking app below 1kB in a single file.  
 <img src="zero1.png" width="400"><img src="zero2.png" width="400">
 
 This is a no-database note taking application, that is very simple. It just lets you to write a note with name and lists them on the main page.
 With the first note you write, the "n/" directory will be created and every note will be there in form of a text file. This gives you the ability
-to perform actions on your notes with whatever utilities you like (UNIX philosophy - Do just one thing and do it well). It has exactly 988 bytes when 
+to perform actions on your notes with whatever utilities you like. It has exactly 857 bytes when 
 compressed.
 
-On daily basis, this might be too simple for your needs. Thats why the zeronote repository has multiple branches:
- - __zeronote/main__ - the most minimal barebone writing and listing, 988 bytes
- - __zeronote/simple__ - (main) + renaming and note deletion _Currently Work-in-Progress_
- - __zeronote/basic__ - (simple) + listing timestamps and tagging system _Currently Work-in-Progress_
- - __zeronote/regular__ - (basic) + search, password protection of the whole notepad, sharing notes with URL _Currently Work-in-Progress_
+I'm working on a little more robust version now, in the scheme of the same philosophy, but with more fundamental features. Check in here later on to find out.
--- index.php
+++ index.php
@@ -1,23 +1,34 @@
+<?php if(isset($_POST["n"])){$n=$_POST["n"];$c=$_POST["c"];if(file_put_contents("n/".$n,$c)){header("Location: /");}}?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <title>Boku</title>
   <style>
-      html,body{height:100%}body{max-width:700px;margin:0 auto;padding:12px;font:14px/19px sans-serif;color:#2a3fa8}
-      body,a{box-sizing:border-box}
-      a{color:#2a3fa8;text-decoration:none}
-      a::after{content:"";display:block}
+    html,body{height:100%}
+    body,a,textarea,form,input{box-sizing:border-box;margin:0;padding:0}
+    body{max-width:700px;margin:0 auto;padding:12px;font:14px/19px sans-serif;color:#2a3fa8}
+    a{color:#2a3fa8;text-decoration:none;height:19px}
+    a::after{content:"";display:block}
+    form{height:100%}
+    form>*{outline:0}
+    #n{width:170px;height:19px;border:1px solid #ccc;font:12px/15px Verdana,sans-serif;padding:4px}
+    #s{background:none;color:#2e3fa8;border:0;font:inherit;height:19px;cursor:pointer}
+    textarea{width:100%;height:calc(100% - 29px);resize:none;padding:9px;font:inherit;margin-top:10px;border:1px solid #ccc}
+    textarea:focus,#name:focus{border:1px solid #2e3fa8}
+    form>*:not(textarea){margin-right:18px}
   </style>
 </head>
 <body>
-    <a href="w.php">+ Write</a><br>
-    <?php
-    if (!file_exists("n")){mkdir("n");}
-    else{foreach(glob("n/*") as $f) {
-            echo "<a href='w.php?r=".$f."'>".basename($f)."</a>";
-        }
-    }
-    ?>
+    <?php if(isset($_GET["r"])){$r=$_GET["r"];$f=file_get_contents($r);?>
+    <form action="" method="post">
+    <input type="text" id="n" name="n" value="<?=basename($r);?>">
+    <input type="submit" value="Save" id="s">
+    <a href="/">Exit</a>
+    <textarea name="c"><?=$f;?></textarea>
+    </form>
+    <?php }else{?>
+    <a href="?r">+ Write</a><br>
+    <?php if(!file_exists("n")){mkdir("n");}else{foreach(glob("n/*") as $f){echo"<a href='?r=".$f."'>".basename($f)."</a>";}}}?>
 </body>
 </html>
